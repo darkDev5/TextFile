bfb407bc15f5f172922c81aebf8890f7a78cf43b
darkDev5 <mehdilavasani79@gmail.com>
2023-03-26 14:23:11 +0430

    CountType enum

---
+++ src/org/darkdev5/lib/file/text/option/CountType.java
@@ -0,0 +1,14 @@
+package org.darkdev5.lib.file.text.option;
+
+/**
+ * @author darkDev5
+ * @version 2.0
+ * @since 1.8
+ */
+public enum CountType {
+    Line,
+    EmptyLine,
+    Word,
+    Character,
+    Spaces
+}
